bdda373a2353f43b2a50d73dc8355ad2bc5f1e07
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2021-02-21 23:18:11 +0200

    add project skelton

---
+++ src/main/java/com/turchyn/lab2/Lab2CrudRestApplication.java
@@ -0,0 +1,13 @@
+package com.turchyn.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2CrudRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2CrudRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turchyn/lab2/Lab2CrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turchyn.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2CrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
